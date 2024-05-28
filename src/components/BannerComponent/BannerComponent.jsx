6bd264f6fefa57b757075606682679ac8860a597
yessenia22c <yesseniavillarte@gmail.com>
2024-05-28 13:48:16 -0400

    chore: Add z-index to BannerComponent image for proper layering

--- src/components/BannerComponent/BannerComponent.jsx
+++ src/components/BannerComponent/BannerComponent.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const BannerComponent = () => {
   return (
-    <img className='w-full h-screen  object-cover ' src="./src/assets/fondo.jpg" alt="" />
+    <img className='w-full h-screen  object-cover z-10 ' src="./src/assets/fondo.jpg" alt="" />
   )
 }
