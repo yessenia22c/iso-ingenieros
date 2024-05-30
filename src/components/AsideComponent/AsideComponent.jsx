070d2ba2f3bd2565d33928e5dab40a59783a6b10
yessenia22c <65354520+yessenia22c@users.noreply.github.com>
2024-05-30 19:05:29 -0400

    chore: Refactor AsideComponent to include close button and responsive design

--- src/components/AsideComponent/AsideComponent.jsx
+++ src/components/AsideComponent/AsideComponent.jsx
@@ -1,30 +1,53 @@
-import React from 'react'
+ import { useState } from "react";
 
-export const AsideComponent = ({isHidden}) => {
-    console.log(isHidden +' DESE ASIDE');
+// eslint-disable-next-line react/prop-types
+export const AsideComponent = ({isHidden, isMinWidth}) => {
+    console.log('VALOR HIDEN ', isHidden +' VALOR MIN WIDTH ', isMinWidth);
+
+     const [cerrarSidebar, setCerrarSidebar] = useState(false);
+    // const [isMinWidth, setIsMinWidth] = useState(window.innerWidth <= 500);
+    // console.log(isMinWidth);
+    // useEffect(() => {
+    // const handleResize = () => {
+    //     setIsMinWidth(window.innerWidth <= 500);
+    // }
+    // window.addEventListener('resize', handleResize);
+    // return () => window.removeEventListener('resize', handleResize);
     
-        if(isHidden){
-            return (
-               
-             <aside className=' sm:flex flex-col w-64 bg-gray-800 text-white top-0 z-30'>
-                <ul className='flex flex-col gap-4 p-4'>
-                    <li>
-                        <a href='#' className='text-white'>Inicio</a>
-                    </li>
-                    <li>
-                        <a href='#' className='text-white'>Acerca de</a>
-                    </li>
-                    <li>
-                        <a href='#' className='text-white'>Cursos</a>
-                    </li>
-                    <li>
-                        <a href='#' className='text-white'>Contactos</a>
-                    </li>
-                </ul>
-             </aside>
+    // }, []);
+    const bntCerrarSidebar = () => {
+        setCerrarSidebar(!cerrarSidebar);
+        console.log('CERRAR SIDEBAR ', isHidden);
+    }
+    if(isHidden && isMinWidth){
+        return (
+            
+            <aside className='min-[500px]:hidden flex-col w-64 bg-gray-800 text-white top-0 h-full fixed z-30'>
+                <div className="flex mx-4 justify-between my-4">
+                    <img className='w-[60px] p-0' src="./src/assets/ISO-INGENIEROS-LOGO.svg" alt="" />
+                    <button onClick={bntCerrarSidebar}>
+                        <img src="./src/assets/iconos/close.svg" alt="" />
+                    </button>
+                </div>
+                
+            <ul className='flex flex-col gap-4 p-4'>
+                <li>
+                    <a href='#' className='text-white'>Inicio</a>
+                </li>
+                <li>
+                    <a href='#' className='text-white'>Acerca de</a>
+                </li>
+                <li>
+                    <a href='#' className='text-white'>Cursos</a>
+                </li>
+                <li>
+                    <a href='#' className='text-white'>Contactos</a>
+                </li>
+            </ul>
+            </aside>
 
-        
-            )
-        }
+    
+        )
+    }
     
 }
