7838536d66043e417e10585d2f0f65b3cf9cb837
yessenia22c <yesseniavillarte@gmail.com>
2024-05-31 15:29:55 -0400

    Refactor AsideComponent to include close button and responsive design

--- src/components/AsideComponent/AsideComponent.jsx
+++ src/components/AsideComponent/AsideComponent.jsx
@@ -1,49 +1,40 @@
- import { useState } from "react";
-
+ import { useEffect, useState } from "react";
 // eslint-disable-next-line react/prop-types
-export const AsideComponent = ({isHidden, isMinWidth}) => {
+export const AsideComponent = ({isHidden, isMinWidth, close}) => {
     console.log('VALOR HIDEN ', isHidden +' VALOR MIN WIDTH ', isMinWidth);
-
-     const [cerrarSidebar, setCerrarSidebar] = useState(false);
-    // const [isMinWidth, setIsMinWidth] = useState(window.innerWidth <= 500);
-    // console.log(isMinWidth);
-    // useEffect(() => {
-    // const handleResize = () => {
-    //     setIsMinWidth(window.innerWidth <= 500);
-    // }
-    // window.addEventListener('resize', handleResize);
-    // return () => window.removeEventListener('resize', handleResize);
+    const [hiddenAside, setHiddenAside] = useState(true);
     
-    // }, []);
-    const bntCerrarSidebar = () => {
-        setCerrarSidebar(!cerrarSidebar);
-        console.log('CERRAR SIDEBAR ', isHidden);
+    const cerrarAside = () => {
+        close();
+        
     }
+    
     if(isHidden && isMinWidth){
+        
         return (
             
-            <aside className='min-[500px]:hidden flex-col w-64 bg-gray-800 text-white top-0 h-full fixed z-30'>
+            <aside className={hiddenAside? 'min-[500px]:hidden flex-col w-64 bg-gray-800 text-white top-0 h-full fixed z-30': 'hidden'}>
+                {/* <HeaderCerrarAside cerrarAside = {cerrarAside}></HeaderCerrarAside> */}
                 <div className="flex mx-4 justify-between my-4">
                     <img className='w-[60px] p-0' src="./src/assets/ISO-INGENIEROS-LOGO.svg" alt="" />
-                    <button onClick={bntCerrarSidebar}>
+                    <button onClick={cerrarAside}>
                         <img src="./src/assets/iconos/close.svg" alt="" />
                     </button>
                 </div>
-                
-            <ul className='flex flex-col gap-4 p-4'>
-                <li>
-                    <a href='#' className='text-white'>Inicio</a>
-                </li>
-                <li>
-                    <a href='#' className='text-white'>Acerca de</a>
-                </li>
-                <li>
-                    <a href='#' className='text-white'>Cursos</a>
-                </li>
-                <li>
-                    <a href='#' className='text-white'>Contactos</a>
-                </li>
-            </ul>
+                <ul className='flex flex-col gap-4 p-4'>
+                    <li>
+                        <a href='#' className='text-white'>Inicio</a>
+                    </li>
+                    <li>
+                        <a href='#' className='text-white'>Acerca de</a>
+                    </li>
+                    <li>
+                        <a href='#' className='text-white'>Cursos</a>
+                    </li>
+                    <li>
+                        <a href='#' className='text-white'>Contactos</a>
+                    </li>
+                </ul>
             </aside>
 
     
