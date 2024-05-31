14bed955575520c888989544252cda4bf3fc863d
yessenia22c <yesseniavillarte@gmail.com>
2024-05-31 15:30:15 -0400

    feat: Add CardsComponent for displaying professional competencies

---
+++ src/components/MainComponent/CardsComponent.jsx
@@ -0,0 +1,78 @@
+import React from 'react'
+export const CardsComponent = () => {
+  return (
+    <div className="w-[1000px]  grid grid-col-4 grid-flow-col gap-4">
+      <div className=' flex flex-col items-center'>
+        <img
+          className="w-[80px] h-40 text-red-800"
+          src="./src/assets/iconos/book.svg"
+          alt=""
+        />
+        <h2 className="text-center text-2xl text-gray-900 font-medium my-2">COMPETENCIAS PROFESIONALES</h2>
+        <p>
+          Servicios de Formación enfocados en la Gestión Empresarial, Control de
+          Gestión, Mejora de Procesos y Normas Internacionales ISO, FSSC, BRC,
+          NFPA y otras. Identificamos las necesidades del entorno empresarial y
+          optamos por brindarle herramientas y técnicas avanzadas.
+        </p>
+      </div>
+      <div className=' flex flex-col items-center'>
+        <img
+          className="w-[80px] h-40 text-red-800"
+          src="./src/assets/iconos/book.svg"
+          alt=""
+        />
+        <h2 className="text-center text-2xl text-gray-900 font-medium my-2">COMPETENCIAS PROFESIONALES</h2>
+        <p>
+          Servicios de Formación enfocados en la Gestión Empresarial, Control de
+          Gestión, Mejora de Procesos y Normas Internacionales ISO, FSSC, BRC,
+          NFPA y otras. Identificamos las necesidades del entorno empresarial y
+          optamos por brindarle herramientas y técnicas avanzadas.
+        </p>
+      </div>
+
+      <div className=' flex flex-col items-center'>
+        <img
+          className="w-[80px] h-40 text-red-800"
+          src="./src/assets/iconos/book.svg"
+          alt=""
+        />
+        <h2 className="text-center text-2xl text-gray-900 font-medium my-2">COMPETENCIAS PROFESIONALES</h2>
+        <p>
+          Servicios de Formación enfocados en la Gestión Empresarial, Control de
+          Gestión, Mejora de Procesos y Normas Internacionales ISO, FSSC, BRC,
+          NFPA y otras. Identificamos las necesidades del entorno empresarial y
+          optamos por brindarle herramientas y técnicas avanzadas.
+        </p>
+      </div>
+      <div className=' flex flex-col items-center'>
+        <img
+          className="w-[80px] h-40 text-red-800"
+          src="./src/assets/iconos/book.svg"
+          alt=""
+        />
+        <h2 className="text-center text-2xl text-gray-900 font-medium my-2">COMPETENCIAS PROFESIONALES</h2>
+        <p>
+          Servicios de Formación enfocados en la Gestión Empresarial, Control de
+          Gestión, Mejora de Procesos y Normas Internacionales ISO, FSSC, BRC,
+          NFPA y otras. Identificamos las necesidades del entorno empresarial y
+          optamos por brindarle herramientas y técnicas avanzadas.
+        </p>
+      </div>
+      <div className=' flex flex-col items-center'>
+        <img
+          className="w-[80px] h-40 text-red-800"
+          src="./src/assets/iconos/book.svg"
+          alt=""
+        />
+        <h2 className="text-center text-2xl text-gray-900 font-medium my-2">COMPETENCIAS PROFESIONALES</h2>
+        <p>
+          Servicios de Formación enfocados en la Gestión Empresarial, Control de
+          Gestión, Mejora de Procesos y Normas Internacionales ISO, FSSC, BRC,
+          NFPA y otras. Identificamos las necesidades del entorno empresarial y
+          optamos por brindarle herramientas y técnicas avanzadas.
+        </p>
+      </div>
+    </div>
+  );
+}
