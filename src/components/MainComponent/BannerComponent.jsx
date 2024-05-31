59ab07fc0db29405eed26fbd8ab71f979c5099f5
yessenia22c <yesseniavillarte@gmail.com>
2024-05-31 15:30:07 -0400

    feat: Add BannerComponent with background image

---
+++ src/components/MainComponent/BannerComponent.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const BannerComponent = () => {
+  return (
+    <img className='w-full h-screen  object-cover z-10 ' src="./src/assets/fondo.jpg" alt="" />
+  )
+}
